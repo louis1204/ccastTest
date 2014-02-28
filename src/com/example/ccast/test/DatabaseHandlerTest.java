9178b81a30ca278525807de03884e85f99775a17
Louis <louis1204@gmail.com>
2014-02-28 03:45:37 -0800

    Including a deleteFriend test. Fixed up the DBH test.

--- src/com/example/ccast/test/DatabaseHandlerTest.java
+++ src/com/example/ccast/test/DatabaseHandlerTest.java
@@ -1,6 +1,15 @@
 package com.example.ccast.test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import json.JSONParser;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import user.UserFunctions;
 import main.MainActivity;
@@ -8,11 +17,11 @@ import database.DatabaseHandler;
 import android.test.AndroidTestCase;
 import android.test.InstrumentationTestCase;
 
-public class DatabaseHandlerTest extends InstrumentationTestCase {
+public class DatabaseHandlerTest extends AndroidTestCase {
 
 	public void testConstructor()
 	{
-		DatabaseHandler dbh = new DatabaseHandler(getInstrumentation().getContext());
+		DatabaseHandler dbh = new DatabaseHandler(getContext());
 		assertNotNull(dbh);
 	}
 	
@@ -24,20 +33,34 @@ public class DatabaseHandlerTest extends InstrumentationTestCase {
 	
 	public void testUserFunctionsIsUserLoggedIn()
 	{
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("email", "louis"));
+        params.add(new BasicNameValuePair("password", "louis"));
+        JSONObject json = JSONParser.getInstance().makeHttpRequest(
+        		"http://10.0.2.2/login.php", "POST", params);
+        JSONObject json_user;
+		try {
+			json_user = json.getJSONObject("user");
+	        DatabaseHandler db = new DatabaseHandler(getContext());
+	        db.addUser(json_user.getString("uid"), json_user.getString("first_name"), json_user.getString("username"), json_user.getString("email"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		UserFunctions uf = new UserFunctions();
-		assertTrue(uf.isUserLoggedIn(getInstrumentation().getContext()));
+		assertTrue(uf.isUserLoggedIn(getContext()));
 	}
 	
 	public void testUserDetailsNotNull()
 	{
-		DatabaseHandler dbh = new DatabaseHandler(getInstrumentation().getContext());
+		DatabaseHandler dbh = new DatabaseHandler(getContext());
 		HashMap<String, String> userDets = dbh.getUserDetails();
 		assertNotNull(userDets);
 	}
 	
 	public void testUserDetailsUID()
 	{
-		DatabaseHandler dbh = new DatabaseHandler(getInstrumentation().getContext());
+		DatabaseHandler dbh = new DatabaseHandler(getContext());
 		
 		HashMap<String, String> userDets = dbh.getUserDetails();
 		assertEquals("5", userDets.get("uid"));
--- src/com/example/ccast/test/JSONParserTest.java
+++ src/com/example/ccast/test/JSONParserTest.java
@@ -11,6 +11,7 @@ import org.json.JSONException;
 
 import main.FriendsFragment;
 import main.LoginActivity;
+import main.MainActivity;
 import json.JSONParser;
 import android.test.AndroidTestCase;
 import android.util.Log;
@@ -20,44 +21,26 @@ public class JSONParserTest extends AndroidTestCase {
 
 	public void testContructor()
 	{
-		JSONParser jp = new JSONParser();
-		assertNotNull(jp);
+		assertNotNull(JSONParser.getInstance());
 	}
 	
 	public void testJSONLoginNotNull()
 	{
-		JSONParser jp = new JSONParser();
-		
-		List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("email", "louis"));
-        params.add(new BasicNameValuePair("password", "louis"));
-
-        Log.d("request!", "starting");
-        // getting product details by making HTTP request
-        JSONObject json = jp.makeHttpRequest(
-               "http://10.0.2.2/login.php", "POST", params);
-        assertNotNull(json);
-
-	}
-	
-	public void testJSONLoginSucess()
-	{
-		JSONParser jp = new JSONParser();
 		int success = 0;
-		
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("email", "louis"));
         params.add(new BasicNameValuePair("password", "louis"));
 
         Log.d("request!", "starting");
         // getting product details by making HTTP request
-        JSONObject json = jp.makeHttpRequest(
+        JSONObject json = JSONParser.getInstance().makeHttpRequest(
                "http://10.0.2.2/login.php", "POST", params);
         try{
         	success = json.getInt(LoginActivity.TAG_SUCCESS);
         } catch(JSONException e) {
         	e.printStackTrace();
         }
+        assertNotNull(json);
         assertEquals(1, success);
 	}
 	
@@ -89,31 +72,44 @@ public class JSONParserTest extends AndroidTestCase {
 	
 	public void testJSONArrayGetFriends()
 	{
-		JSONParser jp = new JSONParser();
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("uid", "5"));
 		JSONArray ja = null;
-		ja = jp.makeHttpRequestForFriendsArray("http://10.0.2.2/get_friends.php", "POST", params);
+		ja = JSONParser.getInstance().makeHttpRequestForFriendsArray("http://10.0.2.2/get_friends.php", "POST", params);
 		assertNotNull(ja);
 	}
 	
 	public void testJSONArrayGetFriendsLength()
 	{
-		JSONParser jp = new JSONParser();
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("uid", "5"));
 		JSONArray ja = null;
-		ja = jp.makeHttpRequestForFriendsArray("http://10.0.2.2/get_friends.php", "POST", params);
+		ja = JSONParser.getInstance().makeHttpRequestForFriendsArray("http://10.0.2.2/get_friends.php", "POST", params);
 		assertTrue(ja.length() > 0);
 	}
 	
 	public void testJSONArrayGetFriendsExactLength()
 	{
-		JSONParser jp = new JSONParser();
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("uid", "5"));
 		JSONArray ja = null;
-		ja = jp.makeHttpRequestForFriendsArray("http://10.0.2.2/get_friends.php", "POST", params);
+		ja = JSONParser.getInstance().makeHttpRequestForFriendsArray("http://10.0.2.2/get_friends.php", "POST", params);
 		assertEquals(3, ja.length());
+	}
+	
+	public void testJSONDeleteFriend()
+	{
+		int success = 0;
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("uid", "5"));
+        params.add(new BasicNameValuePair("fid", "4"));
+        
+        JSONObject jsonO = JSONParser.getInstance().makeHttpRequest(FriendsFragment.DELETE_FRIEND_URL, "POST", params);
+        try {
+			success = jsonO.getInt("success");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+        assertEquals(success, 1);
 	}
 }
